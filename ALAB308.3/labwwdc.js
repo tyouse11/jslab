213a407009f93b2d423642c40e3327a6caa84f63
tyouse1 <tyouse1@live.com>
2023-11-30 14:39:44 -0500

    in part 2 of the lab I declared a variable that stores the number of columns in each row of data within the CSV so that it accepts any number of columns based on the first row of data each row is its own array with individual enteries for each column with the parent array located at index 0

--- ALAB308.3/labwwdc.js
+++ ALAB308.3/labwwdc.js
@@ -14,30 +14,33 @@ for (const s of str) {
 }
 
 //new code
-// let arr = [`ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`]
-
-// for (const a of arr) {
-//     if (a === "\n") {
-//         continue
-//     }
-   
-// }
-// console.log(arr)
-
-
 //Part 1
 
-let csvArray = [
-    "ID, Name, Occupation, Age", 
-    "42, Bruce, Knight, 41", 
-    "57, Bob, Fry Cook, 19",
-    "63, Blaine, Quiz Master, 58",
-    "98, Bill, Doctor's Assistant, 26"  
-  ];
-  
-  
-  console.log(csvArray);
+let csvArray = [`ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`]
+
+for (let i = 0; i < csvArray.length; i++){
+    console.log(csvArray[i])
+}
   
 
   //Part 2
+  //Declare a variable that stores the number of columns in each row of data within the CSV.
+ 
+  let csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+
+  // Split the CSV data into rows based on newline character '\n'
+  let rows = csvData.split('\n');
   
+  // Initialize a two-dimensional array to store CSV data
+  let csvArray2 = [];
+  
+  // Process each row to split columns and store in the array
+  rows.forEach(row => {
+    let columns = row.split(','); // Split each row into columns based on comma ','
+    csvArray2.push(columns);
+  });
+  
+  console.log(csvArray2);
+
+
+  //Part 3
